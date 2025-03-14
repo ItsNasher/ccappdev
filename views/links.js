18533769726a16606cf1803d04e80466a7666f7c
Ethan Dela Cruz <165283236+ItsNasher@users.noreply.github.com>
2025-03-15 00:34:30 +0800

    aboutus and routes complete

---
+++ views/links.js
@@ -0,0 +1,7 @@
+function goToCreatePost() {
+    window.location.href = 'createpost.html';
+}
+
+document.getElementById('profile-button').addEventListener('click', function() {
+    window.location.href = '/profile';
+});
